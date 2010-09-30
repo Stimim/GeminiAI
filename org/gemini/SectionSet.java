1d88fe746d753f79027bdeebafc700db1d317873
Stimim <death1048576@gmail.com>
2010-09-30 23:07:59 +0800

    Add new file SectionSet.java

---
+++ org/gemini/SectionSet.java
@@ -0,0 +1,33 @@
+/**
+ *   This file is part of GeminiAI.
+ *   GeminiAI is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   GeminiAI is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with GeminiAI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   @author stimim
+ *   email:  death1048576@gmail.com
+ */
+
+package org.gemini;
+
+import java.util.HashSet;
+
+public final class SectionSet extends HashSet<Section> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void mergeAssumption() {
+		
+	}
+}
