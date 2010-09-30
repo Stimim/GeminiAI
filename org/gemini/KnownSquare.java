5f525a0584acea8677de506a2e2aefb0c2403a0b
Stimim <death1048576@gmail.com>
2010-09-30 09:43:06 +0800

    Add GNU GPL license.

--- org/gemini/KnownSquare.java
+++ org/gemini/KnownSquare.java
@@ -1,3 +1,20 @@
+/*   This file is part of GeminiAI.
+ *   GeminiAI is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   GeminiAI is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Gemini.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Author: Stimim
+ *   Email : death1048576@gmail.com
+ */
 package org.gemini;
 
 public class KnownSquare extends Square {
